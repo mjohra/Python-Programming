781686c00d58cc396f5a0cb2521c421dee6bb773
Your Name <mehjabin.johra00@gmail.com>
2026-05-11 20:16:34 +0600

    data type code

--- DataType.py
+++ DataType.py
@@ -1,4 +1,28 @@
 # String
-
 name = "Mehjabin Johra"
 print(name)
+
+# integer
+age = 30
+print(age)
+
+#checking data type
+print(type(age))
+
+# floating
+amount = 520.5
+print(amount)
+print(type(amount))
+
+#complex
+husband = 1j
+print(husband)
+print(type(husband))
+
+# str type data
+firstName = "Mehjabin"
+lastName = "Johra"
+print("Myself" +' '+firstName +' '+ lastName)
+
+
+
