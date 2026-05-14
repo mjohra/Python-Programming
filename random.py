83118c426e81fae75e0e1fb5ba612d1b4f7e3485
Your Name <mehjabin.johra00@gmail.com>
2026-05-14 12:08:44 +0600

    operator in python

---
+++ random.py
@@ -0,0 +1,30 @@
+a = 1
+b = 2
+c = 3
+d = 4
+#addition
+print (a+b)
+
+#subtraction
+print(b-a)
+
+#multiplication
+print(a*b)
+
+#division
+print(d/b)
+
+#modulas
+print(d%b)
+
+#exponentiation
+x=10
+y=2
+
+print(x ** y) # 10*10
+
+#floor division
+m = 15
+n = 2
+
+print (m // n)
